7f2df6157ac4deabbb5552cd520f057163b771fa
theopen4 <kielze86@gmail.com>
2023-02-23 16:20:13 +0100

    add all element of the json pieces

--- pieces.js
+++ pieces.js
@@ -2,26 +2,40 @@
 const reponse =  await fetch('pieces-autos.json');
 const pieces =  await reponse.json();
 const article = pieces[0];
-const imageElement =  document.createElement('img');
-imageElement.src = article.image;
-const nomElement = document.createElement('h2');
-nomElement.innerText = article.nom;
-const prixElement = document.createElement('p');
-prixElement.innerText = `prix: ${article.prix} $ (${article.prix < 35 ? "$" : "$$"})`;
-const categorieElement = document.createElement('p');
-categorieElement.innerText = article.categorie ?? "aucune categorie";
-const descriptionElement = document.createElement('p');
-descriptionElement.innerText = article.description ?? "pas de description pour le moment ";
-const disponibilityElement = document.createElement('p');
-disponibilityElement.innerText = `${article.disponible ? "En stock " : "en rupture de stock"}`;
 
-const sectionFiches = document.querySelector('.fiches')
-sectionFiches.appendChild(imageElement); 
-sectionFiches.appendChild(nomElement); 
-sectionFiches.appendChild(prixElement);  
-sectionFiches.appendChild(categorieElement); 
-sectionFiches.appendChild(descriptionElement); 
-sectionFiches.appendChild(disponibilityElement); 
+for(let i = 0; i < pieces.length; i++) {
+    //recovery of  the dom  element  that will hosts the files
+    const sectionFiches = document.querySelector('.fiches')
+    //creation of the tag dedicated to an automative part
+    const pieceElement = document.createElement('article');
+    // we create the img element
+    const imageElement = document.createElement('img');
+    // we access the index i of the part list to configure the source of the image
+    imageElement.src = `${pieces[i].image}  `
+    // dido for the name, price and categorie
+    const nomElement = document.createElement('h2');
+    nomElement.innerText = pieces[i].nom;
+    const prixElement = document.createElement('p');
+    prixElement.innerText = `${pieces[i].prix} (${pieces[i].prix < 35 ? "$" : "$$"}) `
+    const categorieElement = document.createElement('p');
+    categorieElement.innerText = pieces[i].categorie ?? "aucune categorie";
+    const descriptionElement = document.createElement('p');
+    descriptionElement.innerText = pieces[i].description ?? "pas de description";
+    const disponibilityElement = document.createElement('p');
+    disponibilityElement.innerText = `${pieces[i].disponible ? "En stock " : "en rupture de stock"}`;
+    sectionFiches.appendChild(pieceElement); 
+    pieceElement.appendChild(imageElement);
+    pieceElement.appendChild(nomElement); 
+    pieceElement.appendChild(prixElement);  
+    pieceElement.appendChild(categorieElement); 
+    pieceElement.appendChild(descriptionElement); 
+    pieceElement.appendChild(disponibilityElement);
+
+
+}
+
+
+console.log(pieces)
 
 
 
